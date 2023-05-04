cf66026d63b443de0b5f98244f045741d711fc96
lizeth montoya <lizethmontoyagarcia@gmail.com>
2023-05-04 14:32:53 -0500

    Realizando el main

--- workshop2-landingpage/src/components/App/App.jsx
+++ workshop2-landingpage/src/components/App/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from '../Header/Header';
 import GlobalStyles from './StylesApp';
+import Main from '../Main/Main';
 
 const App = () => {
   return (
     <>
       <GlobalStyles/>
       <Header/>
+      <Main/>
+      
     </>
   )
 }
