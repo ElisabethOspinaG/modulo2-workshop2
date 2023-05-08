3de77cd9feb19f8bf99bd8e5b557d8725df27de4
elisabeth ospina <elisabethospinag@gmail.com>
2023-05-08 09:52:46 -0500

    estilo responsivo footer

--- workshop2-landingpage/src/components/Footer/StyledFooter.jsx
+++ workshop2-landingpage/src/components/Footer/StyledFooter.jsx
@@ -7,23 +7,54 @@ export const Footerstyled = styled.footer`
   justify-content: space-between;
   align-items: center;
 
+  @media screen and (max-width:375px){
+
+    width: 1500px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+  }
+
     figure{
         display: flex;
         justify-content: end;
         gap: 18px;
         
+        
+        @media screen and (max-width: 375px){
+            padding-top: 30px;
+            margin-bottom: 30px;
+            display: flex;
+            justify-content: center;
+            gap: 30px;
+                          
+        }
+
+
     }
+
+
     p{
         color: white;
         font-family: 'Alata', sans-serif;
+        @media screen and (max-width: 375px){
+           font-size: 2rem;
+                
+        }
     }
-
 `;
 export const DivLogoNav = styled.div`
     display: flex;
     flex-direction: column;
     margin: 40px 0px ;
     gap: 10px;
+
+    /* @media screen and (max-width: 375px) {
+      width: 35rem;
+    } */
+
+
 `;
 export const DivRedes = styled.div`
     display: flex;
