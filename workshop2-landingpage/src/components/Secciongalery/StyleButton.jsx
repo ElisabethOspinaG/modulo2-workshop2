880e518f8d067fddbc7df0db5904718533d85d98
Yesid Vanegas <yesidvanegas68@gmail.com>
2023-05-08 15:27:09 -0500

    trabajando en el boton de ver todo para el responsive

--- workshop2-landingpage/src/components/Secciongalery/StyleButton.jsx
+++ workshop2-landingpage/src/components/Secciongalery/StyleButton.jsx
@@ -4,24 +4,23 @@ export const ButtonStyle = styled.div `
 
     display: none;
     
-    @media screen and (max-width: 765px) {
-
-        width: 1100px;
-        align-items: center;
-        margin: 120px 180px 300px 180px;
+    @media screen and (max-width: 380px) {
+        width: 100%;
         display: flex;
         justify-content: center;
+        align-items: center;
+        margin-bottom: 20px;
 
         button {
-            padding: 20px 20px;
-            width: 25%;
+            padding: 10px 10px;
+            width: 50%;
             background-color: white;
             border-color: black;
             font-size: 20px;
             
-            :hover {
+            :active {
                 background-color: black;
-                transition: background-color 3s ease-out;
+                transition: background-color 0.1s ease-out;
                 color: white;
                 cursor: pointer;
                 
